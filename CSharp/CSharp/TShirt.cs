3057f5c8b5e14b30faa195b065e8859fa400a1c1
Rich Boykin <richboykin@hotmail.com>
2015-10-20 12:36:14 -0400

    C# and F# code added

---
+++ CSharp/CSharp/TShirt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class TShirt
+    {
+
+        //Equal Rights
+        public void equalRights ()
+        {
+            var reject = 
+                new string[]
+                {
+                    "Racism", "Sexism", "Ageism", "Homophobia"
+                };
+            foreach(var x in reject)
+            {
+                Console.WriteLine("I reject " + x + "!");
+            }
+        } 
+
+        //I Love My Family
+        public string iLove ()
+        {
+            return "My Family";
+        }
+
+        //I Respect The Elderly
+        public string iRespect ()
+        {
+            return "The Elderly";
+        }
+
+        // I Support The Youth
+        public string iSupport ()
+        {
+            return "The Youth";
+        }
+
+        //Playtime Or Nah
+        public void playTimeOrNah (string input)
+        {
+            string answer;
+
+            switch (input.ToLower())
+            {
+                case "legos":
+                    answer = "Play Time";
+                    break;
+                case "xbox":
+                    answer = "Play Time";
+                    break;
+                case "vegetables":
+                    answer = "No comment.";
+                    break;
+                default:
+                    answer = "Play Time";
+                    break;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ CSharp/TShirt/TShirts.fs
@@ -0,0 +1,35 @@
+﻿module TShirts
+
+open System
+
+module AllShirts =
+    
+    // Equal Rights
+    let equalRights () =
+        let reject = 
+            [|"Racism"; "Sexism"; 
+            "Ageism"; "Homophobia"|]
+
+        for x in reject do
+            printfn "I Reject %s!" x
+
+    // I Love My Family
+    let iLove () =
+        "My Family"
+
+    // I Respect The Elderly
+    let iRespect () =
+        "The Elderly"
+
+    // I Support The Youth
+    let iSupport () =
+        "The Youth"
+
+    // Playtime Or Nah
+    let playTimeOrNah (input: string) =
+        
+        match input.ToLower() with
+        | "legos" -> printfn "Play Time"
+        | "xbox" -> printfn "Play Time"
+        | "vegetables" -> printfn "No comment."
+        | _ -> printfn "Play Time"
